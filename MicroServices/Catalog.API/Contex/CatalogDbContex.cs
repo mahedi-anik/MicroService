0e5aa0c2797fcf40e539e3e8648840c97409201b
MH_Anik <65283345+mahedi-anik@users.noreply.github.com>
2023-09-15 22:40:06 +0600

    .

---
+++ MicroServices/Catalog.API/Contex/CatalogDbContex.cs
@@ -0,0 +1,11 @@
+﻿using MongoRepo.Context;
+
+namespace Catalog.API.Contex
+{
+    public class CatalogDbContex : ApplicationDbContext
+    {
+        public CatalogDbContex(string connectionString, string databaseName) : base(connectionString, databaseName)
+        {
+        }
+    }
+}
